579f1ffd214910555f4482c24e4c90c5f7e3d4ca
ProgSelC <ProgSelC@users.noreply.github.com>
2015-08-14 13:30:46 +0300

    Create Main.java

---
+++ multisort-research/Main.java
@@ -0,0 +1,157 @@
+package multisort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+	public static void main(String args[]) {
+		final int[] myArr = new int[60900];
+		int threads = 4;
+
+		Random rnd = new Random();
+
+		for (int i = 0; i < myArr.length; i++) {
+			myArr[i] = (rnd.nextInt(10) + 1);
+		}
+
+		int[] arr1 = Arrays.copyOf(myArr, myArr.length);
+		int[] arr2 = Arrays.copyOf(myArr, myArr.length);
+		int[] arr3 = Arrays.copyOf(myArr, myArr.length);
+		int[] arr4 = Arrays.copyOf(myArr, myArr.length);
+
+		System.out.printf("Sorting Quick time with %d threads: %d ms\n",
+				threads, runQickMulti(arr1, threads));
+
+		System.out.printf("Sorting Quick time with %d thread: %d ms\n", 1,
+				runQickMulti(arr2, 1));
+
+		System.out.printf("Sorting Merge time with %d threads: %d ms\n",
+				threads, runMergeMulti(arr3, threads));
+
+		System.out.printf("Sorting Merge time with %d thread: %d ms\n", 1,
+				runMergeMulti(arr4, 1));
+
+		long rTime = System.currentTimeMillis();
+
+		Arrays.sort(myArr);
+		rTime = System.currentTimeMillis() - rTime;
+
+		System.out.printf("Sorting time with Arrays.sort: %dms\n", rTime);
+
+		System.out.println("Arrays.sort and qick-multi arrays are equal: " + Arrays.equals(arr1, myArr));
+		System.out.println("Arrays.sort and qick-single arrays are equal: " + Arrays.equals(arr2, myArr));
+		System.out.println("Arrays.sort and merge-multi arrays are equal: " + Arrays.equals(arr3, myArr));
+		System.out.println("Arrays.sort and merge-single arrays are equal: " + Arrays.equals(arr4, myArr));
+	}
+
+	// ------------------------------------------------------------------------
+	static long runQickMulti(int[] a, int threads) {
+
+		int pLen = a.length / threads;
+
+		Thread[] thr = new Thread[threads];
+		int[][] pSort = new int[threads][];
+
+		long rTime = System.currentTimeMillis();
+
+		int end = 0;
+		for (int i = 0; i < threads; i++) {
+			end = (i + 1 < threads) ? (i + 1) * pLen : a.length;
+
+			pSort[i] = Arrays.copyOfRange(a, i * pLen, end);
+
+			thr[i] = new Thread(new SortArray(pSort[i]));
+			thr[i].start();
+
+		}
+
+		for (Thread th : thr) {
+			try {
+				th.join();
+			} catch (InterruptedException e) {
+				System.out.println(e);
+			}
+
+		}
+
+		int[] sortedArr = null;
+
+		switch (threads) {
+		case 1:
+			sortedArr = pSort[0];
+			break;
+		case 2:
+			sortedArr = SortArray.merge(pSort[0], pSort[1]);
+			break;
+		case 3:
+			sortedArr = SortArray.merge(SortArray.merge(pSort[0], pSort[1]),
+					pSort[2]);
+			break;
+		case 4:
+			sortedArr = SortArray.merge(SortArray.merge(pSort[0], pSort[1]),
+					SortArray.merge(pSort[2], pSort[3]));
+			break;
+		}
+
+		rTime = System.currentTimeMillis() - rTime;
+
+		System.arraycopy(sortedArr, 0, a, 0, sortedArr.length);
+
+		return rTime;
+	}
+
+	// --------------------------------------------------------------------------
+	static long runMergeMulti(int[] a, int threads) {
+		int pLen = a.length / threads;
+
+		Thread[] thr = new Thread[threads];
+		int[][] pSort = new int[threads][];
+
+		long rTime = System.currentTimeMillis();
+
+		int end = 0;
+		for (int i = 0; i < threads; i++) {
+			end = (i + 1 < threads) ? (i + 1) * pLen : a.length;
+
+			pSort[i] = Arrays.copyOfRange(a, i * pLen, end);
+
+			thr[i] = new Thread(new ArrayThread(pSort[i]));
+			thr[i].start();
+
+		}
+
+		for (Thread th : thr) {
+			try {
+				th.join();
+			} catch (InterruptedException e) {
+				System.out.println(e);
+			}
+
+		}
+
+		int[] sortedArr = null;
+
+		switch (threads) {
+		case 1:
+			sortedArr = pSort[0];
+			break;
+		case 2:
+			sortedArr = SortArray.merge(pSort[0], pSort[1]);
+			break;
+		case 3:
+			sortedArr = SortArray.merge(SortArray.merge(pSort[0], pSort[1]),
+					pSort[2]);
+			break;
+		case 4:
+			sortedArr = SortArray.merge(SortArray.merge(pSort[0], pSort[1]),
+					SortArray.merge(pSort[2], pSort[3]));
+			break;
+		}
+
+		rTime = System.currentTimeMillis() - rTime;
+
+		System.arraycopy(sortedArr, 0, a, 0, sortedArr.length);
+
+		return rTime;
+	}
+}
